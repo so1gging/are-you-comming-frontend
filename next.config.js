21e97117214945dcd439272caacb5575caec5ed6
김솔지 <so1gging@gmail.com>
2023-05-28 15:51:14 +0900

    :sparkles: svgr 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_URL: process.env.API_URL,
-        API_KEY: process.env.API_KEY,
-    }
+  env: {
+    API_URL: process.env.API_URL,
+    API_KEY: process.env.API_KEY,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
