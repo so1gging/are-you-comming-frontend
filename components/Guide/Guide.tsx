a7869bdb64fa1f5d12a0288f57609984aad488f4
김솔지 <so1gging@gmail.com>
2023-05-28 21:01:59 +0900

    :lipstick: [#4] Guide 컴포넌트 추가

--- components/Guide/Guide.tsx
+++ components/Guide/Guide.tsx
@@ -1,17 +1,16 @@
-import React, { FC } from 'react'
+import React, { FC, PropsWithChildren } from 'react'
 import Image from 'next/image'
 import { StaticImageData } from 'next/dist/client/image'
 
 interface GuideProps {
   src: StaticImageData | string | { default: StaticImageData }
   alt: string
-  text: string
 }
-const Guide: FC<GuideProps> = ({ src, alt, text }) => {
+const Guide: FC<PropsWithChildren<GuideProps>> = ({ src, alt, children }) => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-5">
       <Image src={src} alt={alt} width={125} height={125} />
-      <p className="text-center text-h1">{text}</p>
+      <p className="text-center text-h1">{children}</p>
     </div>
   )
 }
