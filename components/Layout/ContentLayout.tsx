f34e88627173e2d36f1fbb403d18df4c5340a198
김솔지 <so1gging@gmail.com>
2023-06-20 22:42:12 +0900

    :lipstick:  [#16] ContentLayout flex 로 지정 믿 수정

--- components/Layout/ContentLayout.tsx
+++ components/Layout/ContentLayout.tsx
@@ -4,7 +4,7 @@ interface ContentLayout {
   backgroundColor?: 'white' | 'bg01'
 }
 const ContentLayout: FC<PropsWithChildren<ContentLayout>> = ({ backgroundColor = 'white', children }) => (
-  <main className={`w-full h-full py-10 bg-${backgroundColor} px-8 rounded-xl`}>{children}</main>
+  <main className={`w-full h-full py-10 bg-${backgroundColor} px-8 rounded-xl flex flex-col gap-10`}>{children}</main>
 )
 
 export default ContentLayout
