7aba4186ae152798c6be3471783d86136f315225
김솔지 <so1gging@gmail.com>
2023-07-04 19:14:06 +0900

    :sparkles: [#26] page에서 api 호출

--- app/bus-station/[id]/page.tsx
+++ app/bus-station/[id]/page.tsx
@@ -1,5 +1,9 @@
+'use client'
 import ContentLayout from '@/components/Layout/ContentLayout'
+import useGetBusArrivalListOut from '@/lib/apis/bus-arrival-service/api/bus-arrival-service'
 
 export default function Page({ params }: { params: { id: string } }) {
+  const { data } = useGetBusArrivalListOut({ stationId: params.id })
+
   return <ContentLayout backgroundColor="white">{params.id}</ContentLayout>
 }
